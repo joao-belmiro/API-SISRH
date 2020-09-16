f228f089945e4fd878dd77c49198caed281c739d
João Carlos Belmiro Conçalves <14908@adm.bancoamazonia.sa>
2020-09-16 11:38:17 -0300

    impl autenticação de usuario

--- src/main/java/br/cadastro/api/manager/UsuarioManager.java
+++ src/main/java/br/cadastro/api/manager/UsuarioManager.java
@@ -1,5 +1,20 @@
 package br.cadastro.api.manager;
 
-public class UsuarioManager {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import br.cadastro.api.models.Usuario;
+import br.cadastro.api.repository.UsuarioRepository;
+
+@Service
+public class UsuarioManager {
+	
+	@Autowired
+	UsuarioRepository usuarioRepository;
+	
+	public Usuario salvarUsuario (Usuario user) {
+		return usuarioRepository.save(user);
+	}
+
+	
 }
