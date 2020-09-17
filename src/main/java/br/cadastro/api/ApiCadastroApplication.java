fe37764a553494d23cfdcd3769d3a3de68c1f6e2
João Carlos Belmiro Conçalves <14908@adm.bancoamazonia.sa>
2020-09-17 13:03:13 -0300

    configurando crosorigin

--- src/main/java/br/cadastro/api/ApiCadastroApplication.java
+++ src/main/java/br/cadastro/api/ApiCadastroApplication.java
@@ -2,6 +2,10 @@ package br.cadastro.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
 @SpringBootApplication
@@ -9,5 +13,16 @@ public class ApiCadastroApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiCadastroApplication.class, args);
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+	    return new WebMvcConfigurerAdapter() {
+	        @Override
+	        public void addCorsMappings(CorsRegistry registry) {
+	            registry.addMapping("/**").allowedOrigins("http://localhost:8080/home");
+	        }
+	    };
 	}
 }
