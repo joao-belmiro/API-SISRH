c29218db29fb17199d4be95e79012df4732314bf
João Carlos Belmiro Conçalves <14908@adm.bancoamazonia.sa>
2020-09-18 13:45:55 -0300

    rmv pacotes não usados

--- src/main/java/br/cadastro/api/configuration/secutiryConfig.java
+++ src/main/java/br/cadastro/api/configuration/secutiryConfig.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -39,15 +38,9 @@ public class secutiryConfig extends WebSecurityConfigurerAdapter {
 	public OncePerRequestFilter jwtFilter () {
 		return new JwtAuthFilter(jwtService, usuarioManager);
 	}
-		
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(usuarioManager).passwordEncoder(PasswordEncoder());
-
-	}
 	
 	@Bean
-	CorsConfigurationSource corsConfigurationSource () {
+	public CorsConfigurationSource corsConfigurationSource () {
 		CorsConfiguration configuration = new CorsConfiguration();
 		configuration.setAllowCredentials(true);
 		configuration.setAllowedHeaders(Arrays.asList("Access-Control-Allow-Headers","Access-Control-Allow-Origin","Access-Control-Request-Method", "Access-Control-Request-Headers","Origin","Cache-Control", "Content-Type", "Authorization"));
