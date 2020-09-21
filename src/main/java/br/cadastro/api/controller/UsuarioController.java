938f299509a4b2114e68dabed49b783ff7a16cbb
João Carlos Belmiro Conçalves <14908@adm.bancoamazonia.sa>
2020-09-21 13:10:55 -0300

    impl crud usuario

--- src/main/java/br/cadastro/api/controller/UsuarioController.java
+++ src/main/java/br/cadastro/api/controller/UsuarioController.java
@@ -1,22 +1,29 @@
 package br.cadastro.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import br.cadastro.api.dto.CredenciaisDto;
 import br.cadastro.api.dto.TokenDto;
+import br.cadastro.api.dto.UsuarioDto;
 import br.cadastro.api.exceptions.CredenciaisIncorretasException;
 import br.cadastro.api.manager.UsuarioManager;
 import br.cadastro.api.models.Usuario;
+import br.cadastro.api.repository.projections.UserData;
 import br.cadastro.api.security.JwtService;
 
 @RestController
@@ -40,6 +47,17 @@ public class UsuarioController {
 		Usuario user  = usuarioManager.salvarUsuario(usuario);
 		
 		return new ResponseEntity<Usuario>(user, HttpStatus.CREATED);
+	}
+	
+	public ResponseEntity<UsuarioDto> alterar (@RequestBody UsuarioDto UsuarioDto) {
+		usuarioManager.alterar(UsuarioDto);
+		return new ResponseEntity<UsuarioDto>(HttpStatus.NO_CONTENT);
+	}
+	
+	@GetMapping("tag")
+	public ResponseEntity<List<UserData>> filtro(@RequestParam(required = true) String tag) {
+		List<UserData> users = usuarioManager.buscarPorTag(tag);
+		return new ResponseEntity<List<UserData>>(users, HttpStatus.OK);
 	}
 	
 	@PostMapping("autenticar")
--- src/main/java/br/cadastro/api/manager/UsuarioManager.java
+++ src/main/java/br/cadastro/api/manager/UsuarioManager.java
@@ -1,6 +1,10 @@
 package br.cadastro.api.manager;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -8,10 +12,13 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
+import br.cadastro.api.dto.UsuarioDto;
 import br.cadastro.api.exceptions.CredenciaisIncorretasException;
 import br.cadastro.api.models.Usuario;
 import br.cadastro.api.repository.UsuarioRepository;
+import br.cadastro.api.repository.projections.UserData;
 
 @Service
 public class UsuarioManager implements UserDetailsService {
@@ -23,8 +30,29 @@ public class UsuarioManager implements UserDetailsService {
 	private PasswordEncoder encoder;
 		
 	@Transactional
-	public Usuario salvarUsuario (Usuario user) {
+	public Usuario salvarUsuario (Usuario user) throws UsernameNotFoundException, CredenciaisIncorretasException {
 		return usuarioRepository.save(user);
+	}
+	
+	public void alterar (UsuarioDto usuarioDto)  {
+		Usuario usuarioEncontrado = usuarioRepository.findById(usuarioDto.getId())
+				.orElseThrow(() ->  new UsernameNotFoundException("Usuário não cadastrado na base de dados"));
+		Boolean senhasBatem = encoder.matches(usuarioDto.getSenha(), usuarioEncontrado.getSenha());
+		if (senhasBatem) {
+			Usuario usuario = new Usuario();
+			usuario.setId(usuarioDto.getId());
+			usuario.setLogin(usuarioDto.getLogin());
+			usuario.setAdmin(usuarioDto.getAdmin());
+			usuario.setSenha(usuarioDto.getNovaSenha());
+			usuarioRepository.save(usuario);
+		
+		} else {
+			throw new CredenciaisIncorretasException();
+		}
+	}
+	
+	public List<UserData> buscarPorTag (String tag) {
+		return usuarioRepository.buscarPorTag(tag);
 	}
 	
 	public UserDetails autenticar (Usuario usuario) {
